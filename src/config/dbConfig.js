ecd403a85d77d23c02ae245a60005682c8fe3fcf
josephEmeka <josephemekaonah@gmail.com>
2024-11-11 13:03:49 +0100

    dbconfig-file-changes

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -14,16 +14,3 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${proces
 });
 
 module.exports = sequelize;
-
-
-
-
-sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-
-module.exports = sequelize;
