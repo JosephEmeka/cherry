c959992e4d9ca82b4934dd37b4a7cdda8b1f25a7
josephEmeka <josephemekaonah@gmail.com>
2024-11-11 10:55:13 +0100

    changes to server port

--- src/server.js
+++ src/server.js
@@ -1,17 +1,8 @@
-const app = require('./app');
-const { Pool } = require('pg');
 require('dotenv').config();
 const sequelize = require('./config/dbConfig');
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-});
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PGPORT || 5000;
 
 
 
