f78f4c59afc3df58b665e5feb080c45ae1e24777
josephEmeka <josephemekaonah@gmail.com>
2024-11-18 12:14:18 +0100

    Remove node_modules from tracking

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const cors = require("cors");
 
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(userRoutes);
+
 
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
